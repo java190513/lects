8eaabf8134692e0636788eedaa533f7a76d24150
Paulius Zubavičius <paulius@zubavicius.lt>
2019-05-13 15:49:43 +0300

    Hello world added

--- l01_hello_world/src/main/java/com/vcs/lects/l01/HelloWorld.java
+++ l01_hello_world/src/main/java/com/vcs/lects/l01/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Labas pasauli :) !!!");
+		System.out.println("Hello world");
+
+		System.out.println("Labas pasauli :) !!! ");
 		
 		
 
