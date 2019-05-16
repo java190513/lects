c44e539003ce284fdac2a80f2d7d72a5ff7b8321
Paulius Zubavičius <paulius@zubavicius.lt>
2019-05-16 13:22:29 +0300

    tasks

---
+++ l04_operators_ifs_switches/src/main/java/com/vcs/lects/l04/operators/tasks/AritmetineProgresija.java
@@ -0,0 +1,10 @@
+package com.vcs.lects.l04.operators.tasks;
+
+public class AritmetineProgresija {
+
+	public double formule(double a, double d, double n) {
+		// TODO
+		return 0;
+	}
+
+}
---
+++ l04_operators_ifs_switches/src/main/java/com/vcs/lects/l04/operators/tasks/Sultys.java
@@ -0,0 +1,16 @@
+package com.vcs.lects.l04.operators.tasks;
+
+public class Sultys {
+
+	
+
+	    public boolean arNeraLikucio(
+	                  int sulciuKiekisViso,
+	                  int talposTuris, 
+	                  boolean reikalingasTikLyginis) {
+	        //TODO
+	        return false;
+	    }
+	
+
+}
