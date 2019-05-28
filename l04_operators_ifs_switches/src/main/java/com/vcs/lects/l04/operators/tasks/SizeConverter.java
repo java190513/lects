8d651b5d5d4ad50fa46512f9d3f8c8d5adc3b663
Paulius Zubavičius <paulius@zubavicius.lt>
2019-05-28 17:01:43 +0300

    Task and solutions

---
+++ l04_operators_ifs_switches/src/main/java/com/vcs/lects/l04/operators/tasks/SizeConverter.java
@@ -0,0 +1,20 @@
+package com.vcs.lects.l04.operators.tasks;
+
+public class SizeConverter {
+
+    /**
+     * 0 bytes ...; 1kb ... ; 1Mb... 1Gb ... ; 1Tb and more;
+     * <p>
+     * Example: 1,3Gb // <- Acceptable 893Kb // <- Acceptable 1Gb and 312Mb and
+     * ...// <- Not acceptable
+     * </p>
+     * http://byteconvert.org/
+     */
+
+
+    public String convertToHumanReadable(long sizeInBytes) {
+
+        //TODO
+        return "";
+    }
+}
--- l04_operators_ifs_switches/src/main/java/com/vcs/lects/l04/operators/tasks/Sultys.java
+++ l04_operators_ifs_switches/src/main/java/com/vcs/lects/l04/operators/tasks/Sultys.java
@@ -2,15 +2,24 @@ package com.vcs.lects.l04.operators.tasks;
 
 public class Sultys {
 
-	
 
-	    public boolean arNeraLikucio(
-	                  int sulciuKiekisViso,
-	                  int talposTuris, 
-	                  boolean reikalingasTikLyginis) {
-	        //TODO
-	        return false;
-	    }
-	
+    public boolean arNeraLikucio(
+            int sulciuKiekisViso,
+            int talposTuris) {
+        return sulciuKiekisViso % talposTuris == 0;
+    }
+
+
+    public boolean arNeraLikucio(
+            int sulciuKiekisViso,
+            int talposTuris,
+            boolean reikalingasTikLyginis) {
+
+        boolean arNeraLikucio = sulciuKiekisViso % talposTuris == 0;
+        boolean arLyginis = ((double) sulciuKiekisViso / talposTuris) % 2 == 0;
+
+        return arNeraLikucio && (!reikalingasTikLyginis || arLyginis);
+    }
+
 
 }
